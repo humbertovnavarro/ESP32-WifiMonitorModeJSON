d3e93a5b67b706a6198317e937a497c5db759185
kamaii <humberton.navarro.dev@gmail.com>
2025-07-14 01:47:34 -0700

    refactor + hopping

--- src/main.c
+++ src/main.c
@@ -1,12 +1,17 @@
 #include "esp_wifi.h"
 #include "nvs_flash.h"
 #include "wifi_sniffer.h"
+#define HOP_DELAY 200
 void app_main(void) {
   nvs_flash_init();
   esp_event_loop_create_default();
-  wifi_sniffer_start();
-  for(;;) {
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  uint8_t channels[] = {1, 6, 11};
+  for(int i = 0; i < 3; i++) {
+    esp_wifi_set_channel(channels[i], 0);
+    if(i == 2) {
+      i = 0;
+    }
+    vTaskDelay(HOP_DELAY / portTICK_PERIOD_MS);
   }
 }
 
